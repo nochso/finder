65d3aa5dab23290570f2a30c9dc572aefe25a9ec
Marcel Voigt <mv@noch.so>
2016-10-09 23:01:43 +0200

    Rearrange test helpers

--- finder_test.go
+++ finder_test.go
@@ -5,16 +5,49 @@ import (
 	"testing"
 )
 
-type testCase struct {
-	finder *Finder
-	paths  []string
-}
-
 // test runs a slice of test cases.
 func test(t *testing.T, cases []testCase) {
 	for _, tc := range cases {
 		tc.Test(t)
 	}
+}
+
+type testCase struct {
+	finder *Finder
+	paths  []string
+}
+
+// Test runs a single test case.
+func (tc testCase) Test(t *testing.T) {
+	files, errs := tc.finder.ToSlice()
+	if len(errs) > 0 {
+		t.Fatal(errs)
+	}
+	err := matchFiles(t, tc.paths, files)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// matchFiles asserts that only the expected paths occur in the given files.
+func matchFiles(t *testing.T, paths []string, files []Item) error {
+	m := make(map[string]bool)
+	for _, path := range paths {
+		m[path] = false
+	}
+	for _, file := range files {
+		_, exists := m[file.Path()]
+		if !exists {
+			return fmt.Errorf("unexpected path: %s", file.Path())
+		}
+		m[file.Path()] = true
+	}
+	for path, found := range m {
+		if !found {
+			return fmt.Errorf("missing path: %s", path)
+		}
+	}
+	return nil
 }
 
 func TestIn(t *testing.T) {
@@ -156,37 +189,4 @@ func TestDirs(t *testing.T) {
 		},
 	}
 	test(t, tests)
-}
-
-// Test runs a single test case.
-func (tc testCase) Test(t *testing.T) {
-	files, errs := tc.finder.ToSlice()
-	if len(errs) > 0 {
-		t.Fatal(errs)
-	}
-	err := matchFiles(t, tc.paths, files)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-// matchFiles asserts that only the expected paths occur in the given files.
-func matchFiles(t *testing.T, paths []string, files []Item) error {
-	m := make(map[string]bool)
-	for _, path := range paths {
-		m[path] = false
-	}
-	for _, file := range files {
-		_, exists := m[file.Path()]
-		if !exists {
-			return fmt.Errorf("unexpected path: %s", file.Path())
-		}
-		m[file.Path()] = true
-	}
-	for path, found := range m {
-		if !found {
-			return fmt.Errorf("missing path: %s", path)
-		}
-	}
-	return nil
 }
